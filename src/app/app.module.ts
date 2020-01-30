87031a21b0f342d58fac007f08e7f34be9be55df
alexhatcs <45803329+alexhatcs@users.noreply.github.com>
2020-01-30 14:59:44 -0500

    login/register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,16 @@ import * as ngx from '@ionic-native/ble/ngx';
 import { ToastController } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import firebaseConfig from './firebase'
+import { AngularFireModule } from '@angular/fire'
+import { AngularFireAuthModule } from '@angular/fire/auth'
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
+    ],
   providers: [
     
     ngx.BLE,
